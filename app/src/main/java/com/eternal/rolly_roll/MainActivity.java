5388e0645db50c18ab645d28c564a97cd31a8f74
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-01-13 00:01:31 +0900

    Hello OpenGL
    create openGL window

--- app/src/main/java/com/eternal/rolly_roll/MainActivity.java
+++ app/src/main/java/com/eternal/rolly_roll/MainActivity.java
@@ -6,9 +6,24 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private OpenGLView openGLView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openGLView = (OpenGLView) this.<android.view.View>findViewById(R.id.openGLView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        openGLView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        openGLView.onPause();
     }
 }
---
+++ app/src/main/java/com/eternal/rolly_roll/OpenGLRenderer.java
@@ -0,0 +1,24 @@
+package com.eternal.rolly_roll;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class OpenGLRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        GLES20.glClearColor(1.0f, 0, 0, 1.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+}
