3a915ac69a80d300d30254fb0e43becc0146b3e3
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-01-29 22:06:22 +0900

    Update GPGSManager.java
    need to find documents of GooglePlay library

--- app/src/main/java/com/eternal/rolly_roll/GPGSManager.java
+++ app/src/main/java/com/eternal/rolly_roll/GPGSManager.java
@@ -1,8 +1,30 @@
 package com.eternal.rolly_roll;
 
+import android.content.Context;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.games.AchievementsClient;
+import com.google.android.gms.games.LeaderboardsClient;
+import com.google.android.gms.games.leaderboard.Leaderboard;
 
 public class GPGSManager {
+    private static final String TAG = "GPGS Manager";
+
+    private Context context;
+
     private GoogleSignInClient signInClient;
+    private GoogleSignInAccount account;
+
+    private AchievementsClient achievementsClient;
+    private LeaderboardsClient leaderboardsClient;
+
+    public GPGSManager(Context context) {
+        this.context = context;
+
+        signInClient = GoogleSignIn.getClient(context, new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build());
+
+    }
 }
