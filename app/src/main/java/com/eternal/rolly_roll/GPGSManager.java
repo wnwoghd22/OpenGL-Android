f2163d5ade280a1bfa75c9a2504511fbdee73eb5
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-01-30 14:19:06 +0900

    trying to implement gpgs login

--- app/src/main/java/com/eternal/rolly_roll/GPGSManager.java
+++ app/src/main/java/com/eternal/rolly_roll/GPGSManager.java
@@ -2,13 +2,19 @@ package com.eternal.rolly_roll;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.games.AchievementsClient;
+import com.google.android.gms.games.Games;
 import com.google.android.gms.games.LeaderboardsClient;
 import com.google.android.gms.games.leaderboard.Leaderboard;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class GPGSManager {
     private static final String TAG = "GPGS Manager";
@@ -24,7 +30,21 @@ public class GPGSManager {
     public GPGSManager(Context context) {
         this.context = context;
 
-        signInClient = GoogleSignIn.getClient(context, new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build());
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).build();
+
+        signInClient = GoogleSignIn.getClient(context, gso);
+
+        signInClient.silentSignIn().addOnCompleteListener(new OnCompleteListener<GoogleSignInAccount>() {
+            @Override
+            public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                if (task.isSuccessful()) {
+                    achievementsClient = Games.getAchievementsClient(context, task.getResult());
+                    leaderboardsClient = Games.getLeaderboardsClient(context, task.getResult());
+                } else {
+
+                }
+            }
+        });
 
     }
 }
