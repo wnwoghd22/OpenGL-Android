fd7049d6f986807eac52aa97f17440e76c956609
Jay <wnwoghd22@gmail.com>
2026-01-21 01:59:26 +0900

    update

--- app/src/main/java/com/eternal/rolly_roll/game/model/object/shape/IRenderable.java
+++
@@ -1,7 +0,0 @@
-package com.eternal.rolly_roll.game.model.object.shape;
-
-import com.eternal.rolly_roll.game.view.RenderMiddleware;
-
-public interface IRenderable {
-    public void Render(RenderMiddleware r);
-}
